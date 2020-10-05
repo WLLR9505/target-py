5ab4a543144792f1746f50a63f5e7691b86d3c80
Weuller Jr. Souza <weuller9505@live.com>
2020-10-05 09:18:45 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,14 @@
+from lib.target_menu.colors import palette
+from lib.target_menu import Hmenu
+
+# OK
+# c = palette.selectColor('green')
+# print(f"{c[2]} teste de cor {palette.reset()}")
+# print('FIM')
+
+control = { "pos1" : 1}
+
+
+testMenu = ["item 1", "item 2", "item 3"]
+
+# Hmenu.update(testMenu, control, palette.selectColor('red'), palette)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='target-menu',
+      version='0.0.1',
+      description='Simple target menu with python',
+      url='https://github.com/WLLR9505/target-py',
+      author='Weuller Jr. Souza',
+      license='MIT',
+      packages=['menu'],
+      zip_safe=False)
